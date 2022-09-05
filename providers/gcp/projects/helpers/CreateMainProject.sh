350e3d405041a4df9d664de1953213a26bd62715
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-09-04 22:45:54 -0300

    fix: format fixed

--- providers/gcp/projects/helpers/CreateMainProject.sh
+++ providers/gcp/projects/helpers/CreateMainProject.sh
@@ -13,55 +13,55 @@ export B_ACCOUNT=$2
 
 # Functions
 createServiceAccount() {
-		# Create account
-		gcloud iam service-accounts create ${SA_NAME} \
-		    --description   "${SA_DESCRIPTION}" \
-		    --display-name  ${SA_DISPLAY_NAME} \
-		    --project       ${PROJECT_ID} \
+	# Create account
+	gcloud iam service-accounts create ${SA_NAME} \
+		--description   "${SA_DESCRIPTION}" \
+		--display-name  ${SA_DISPLAY_NAME} \
+		--project       ${PROJECT_ID} \
 		
-		# Set Permission
-		gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-		    --member serviceAccount:${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com \
-		    --role roles/owner
+	# Set Permission
+	gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+		--member serviceAccount:${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com \
+		--role roles/owner
 		
-		# Create and Export key
-		export KEY_NAME="terraform-sa-key"
-		gcloud iam service-accounts keys create ${KEY_NAME}.json \
-		    --iam-account ${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com \
-		    --project ${PROJECT_ID}
+	# Create and Export key
+	export KEY_NAME="terraform-sa-key"
+	gcloud iam service-accounts keys create ${KEY_NAME}.json \
+		--iam-account ${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com \
+		--project ${PROJECT_ID}
 }
 
 createProject() {
-    gcloud projects create ${PROJECT_ID} --name=${PROJECT_NAME} \
-        --set-as-default --folder=$1
+	gcloud projects create ${PROJECT_ID} --name=${PROJECT_NAME} \
+		--set-as-default --folder=$1
 
-    if [ $? -eq 0 ]; then
-        echo "Project ${PROJECT_ID} created"
-    else
-        echo "Project ${PROJECT_ID} not created"
-        exit 1
-    fi
+	if [ $? -eq 0 ]; then
+		echo "Project ${PROJECT_ID} created"
+	else
+		echo "Project ${PROJECT_ID} not created"
+		exit 1
+	fi
 }
 
 setBillingAccount() {
-		gcloud beta billing projects link ${PROJECT_ID} \
-				--billing-account ${B_ACCOUNT}
+	gcloud beta billing projects link ${PROJECT_ID} \
+		--billing-account ${B_ACCOUNT}
 }
 
 createStorage() {
-        # Create bucket to storage terraform state
-       gcloud alpha storage buckets create gs://${PROJECT_ID}-bucket \
-				 --project ${PROJECT_ID}
+	# Create bucket to storage terraform state
+	gcloud alpha storage buckets create gs://${PROJECT_ID}-bucket \
+		--project ${PROJECT_ID}
 }
 
 # Main of Script
 if [ $# -eq 2 ];
 then
-		createProject $1 $2
-		setBillingAccount $1
-		createStorage $1
-		createServiceAccount $1
-    exit 0
+	createProject $1 $2
+	setBillingAccount $1
+	createStorage $1
+	createServiceAccount $1
+	exit 0
 fi
 
 echo "Error: Too many arguments, need only 2 arguments"
