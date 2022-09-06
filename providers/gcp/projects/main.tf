4a5c4fd044d73d0ed3f02144322a51b89f1cf605
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-09-05 22:18:44 -0300

    fix: add absent variables

--- providers/gcp/projects/main.tf
+++ providers/gcp/projects/main.tf
@@ -1,6 +1,9 @@
 module "project" {
-  source = "./modules/project"
-  name = var.product_name
-  environment = var.environment
-  region = var.region
+  source          = "./modules/project"
+  project_id      = var.project_id
+  folder_id       = var.folder_id
+  org_fdqn        = var.org_fdqn
+  org_id          = var.org_id
+  project_name    = var.project_name
+  billing_account = var.billing_account
 }
